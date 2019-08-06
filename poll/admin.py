aa084e59c24e5294949a965345c757c39f56d5a1
Twixes <dev@twixes.com>
2019-08-06 04:52:50 +0200

    Change models

--- poll/admin.py
+++ poll/admin.py
@@ -1,9 +1,29 @@
 from django.contrib import admin
-from poll.models import Teacher, Subject, Group, Assignment, Question, Vote
+from poll.models import Teacher, Subject, Group, Assignment, Question, Completion, Vote
+
+
+class AssignmentInline(admin.TabularInline):
+    model = Assignment
+    extra = 0
+
+
+class VoteInline(admin.TabularInline):
+    model = Vote
+    extra = 0
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('identifier', 'number_of_students')
+    inlines = [AssignmentInline]
+
+
+class CompletionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'group', 'added_at')
+    inlines = [VoteInline]
+
 
 admin.site.register(Teacher)
 admin.site.register(Subject)
-admin.site.register(Group)
-admin.site.register(Assignment)
+admin.site.register(Group, GroupAdmin)
 admin.site.register(Question)
-admin.site.register(Vote)
+admin.site.register(Completion, CompletionAdmin)
--- poll/models.py
+++ poll/models.py
@@ -1,42 +1,45 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
-from django.utils.translation import gettext_lazy as _, pgettext_lazy as p_
+from django.utils.translation import gettext, gettext_lazy, pgettext_lazy
 
 
 class Teacher(models.Model):
     MR = 'MR'
     MS = 'MS'
     TITLE_CHOICES = [
-        (MR, _('Mr.')),
-        (MS, _('Ms.'))
+        (MR, gettext_lazy('Mr.')),
+        (MS, gettext_lazy('Ms.'))
     ]
-    title = models.CharField(max_length=10, choices=TITLE_CHOICES, verbose_name=_('title'))
-    first_name = models.CharField(max_length=50, verbose_name=_('first name'))
-    last_name = models.CharField(max_length=50, verbose_name=_('last name'))
+    title = models.CharField(max_length=10, choices=TITLE_CHOICES, verbose_name=gettext_lazy('title'))
+    first_name = models.CharField(max_length=50, verbose_name=gettext_lazy('first name'))
+    last_name = models.CharField(max_length=50, verbose_name=gettext_lazy('last name'))
 
     class Meta:
-        verbose_name = _('teacher')
-        verbose_name_plural = _('teachers')
+        verbose_name = gettext_lazy('teacher')
+        verbose_name_plural = gettext_lazy('teachers')
         ordering = ['last_name', 'first_name']
 
     def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def get_name_with_title(self):
         return f'{self.get_title_display()} {self.first_name} {self.last_name}'
 
 
 class Subject(models.Model):
     # e.g. "Subject: _English_" or "Przedmiot: _angielski_"
-    name = models.CharField(max_length=50, unique=True, verbose_name=p_('subject name', 'name'))
+    name = models.CharField(max_length=50, unique=True, verbose_name=pgettext_lazy('subject name', 'name'))
     # e.g. "Who teaches you _English_?" or "Kto uczy cię _angielskiego_?"
     name_teacher_selection_case = models.CharField(
-        max_length=50, null=True, verbose_name=_('name in "Who teaches you …?" case (e.g. "English")')
+        max_length=50, null=True, verbose_name=gettext_lazy('name in "Who teaches you …?" case (e.g. "English")')
     )
     teachers = models.ManyToManyField(
-        Teacher, related_name='subjects', related_query_name='subject', verbose_name=_('teachers')
+        Teacher, related_name='subjects', related_query_name='subject', verbose_name=gettext_lazy('teachers')
     )
 
     class Meta:
-        verbose_name = _('subject')
-        verbose_name_plural = _('subjects')
+        verbose_name = gettext_lazy('subject')
+        verbose_name_plural = gettext_lazy('subjects')
         ordering = ['name']
 
     def __str__(self):
@@ -44,17 +47,17 @@ class Subject(models.Model):
 
 
 class Group(models.Model):
-    identifier = models.CharField(max_length=10, verbose_name=_('identifier'))
+    identifier = models.CharField(unique=True, max_length=10, verbose_name=gettext_lazy('identifier'))
     number_of_students = models.PositiveSmallIntegerField(
-        validators=[MinValueValidator(1)], verbose_name=_('number of students')
+        validators=[MinValueValidator(1)], verbose_name=gettext_lazy('number of students')
     )
     subjects = models.ManyToManyField(
-        Subject, related_name='groups', related_query_name='group', verbose_name=_('subjects')
+        Subject, related_name='groups', related_query_name='group', verbose_name=gettext_lazy('subjects')
     )
 
     class Meta:
-        verbose_name = _('group')
-        verbose_name_plural = _('groups')
+        verbose_name = gettext_lazy('group')
+        verbose_name_plural = gettext_lazy('groups')
         ordering = ['identifier']
 
     def __str__(self):
@@ -64,61 +67,80 @@ class Group(models.Model):
 class Assignment(models.Model):
     teacher = models.ForeignKey(
         Teacher, on_delete=models.CASCADE, related_name='assignments', related_query_name='assignment',
-        verbose_name=_('teacher')
-    )
-    group = models.ForeignKey(
-        Group, on_delete=models.CASCADE, related_name='assignments', related_query_name='assignment',
-        verbose_name=_('group')
+        verbose_name=gettext_lazy('teacher')
     )
     subject = models.ForeignKey(
         Subject, on_delete=models.CASCADE, related_name='assignments', related_query_name='assignment',
-        verbose_name=_('subject')
+        verbose_name=gettext_lazy('subject')
+    )
+    group = models.ForeignKey(
+        Group, on_delete=models.CASCADE, related_name='assignments', related_query_name='assignment',
+        verbose_name=gettext_lazy('group')
     )
 
     class Meta:
-        verbose_name = _('assignment')
-        verbose_name_plural = _('assignments')
+        verbose_name = gettext_lazy('assignment')
+        verbose_name_plural = gettext_lazy('assignments')
         constraints = [
-            models.UniqueConstraint(fields=['teacher', 'group', 'subject'], name='unique_assignment')
+            models.UniqueConstraint(fields=['teacher', 'subject', 'group'], name='unique_assignment')
+            # TODO: add teacher_belongs_to_subject and subject_belongs_to_group constraints
         ]
 
     def __str__(self):
-        return f'{self.teacher} – {self.group} – {self.subject}'
+        return f'{self.teacher} – {self.subject} – {self.group}'
 
 
 class Question(models.Model):
-    text = models.CharField(max_length=100, unique=True, verbose_name=_('text'))
+    text = models.CharField(max_length=100, unique=True, verbose_name=gettext_lazy('text'))
     position = models.PositiveSmallIntegerField(
-        unique=True, validators=[MinValueValidator(1)], verbose_name=_('position')
+        unique=True, default=1, validators=[MinValueValidator(1)], verbose_name=gettext_lazy('position')
     )
 
     class Meta:
-        verbose_name = _('question')
-        verbose_name_plural = _('questions')
+        verbose_name = gettext_lazy('question')
+        verbose_name_plural = gettext_lazy('questions')
         ordering = ['position']
 
     def __str__(self):
         return self.text
 
 
-class Vote(models.Model):
-    question = models.ForeignKey(
-        Question, on_delete=models.CASCADE, related_name='votes', related_query_name='vote', verbose_name=_('question')
-    )
-    teacher = models.ForeignKey(
-        Teacher, on_delete=models.CASCADE, related_name='votes', related_query_name='vote', verbose_name=_('teacher')
-    )
+class Completion(models.Model):
     group = models.ForeignKey(
-        Group, on_delete=models.CASCADE, related_name='votes', related_query_name='vote', verbose_name=_('group')
+        Group, on_delete=models.CASCADE, related_name='votes', related_query_name='vote', verbose_name=gettext_lazy('group')
     )
-    mark = models.IntegerField(
-        validators=[MinValueValidator(1), MaxValueValidator(6)], verbose_name=p_('school mark', 'mark')
-    )
-    cast_at = models.DateTimeField(auto_now_add=True, verbose_name=_('cast at'))
+    added_at = models.DateTimeField(auto_now_add=True, verbose_name=gettext_lazy('added at'))
 
     class Meta:
-        verbose_name = _('vote')
-        verbose_name_plural = _('votes')
+        verbose_name = gettext_lazy('completion')
+        verbose_name_plural = gettext_lazy('completions')
 
     def __str__(self):
-        return f'{self.question.position}. – {self.teacher} – {self.group} – {self.mark}'
+        return gettext('completion') + ' ' + gettext('No.') + ' ' + str(self.id)
+
+
+class Vote(models.Model):
+    completion = models.ForeignKey(
+        Completion, on_delete=models.CASCADE,
+        related_name='votes', related_query_name='vote', verbose_name=gettext_lazy('question')
+    )
+    question = models.ForeignKey(
+        Question, on_delete=models.CASCADE, related_name='votes', related_query_name='vote', verbose_name=gettext_lazy('question')
+    )
+    teacher = models.ForeignKey(
+        Teacher, on_delete=models.CASCADE, related_name='votes', related_query_name='vote', verbose_name=gettext_lazy('teacher')
+    )
+    mark = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(6)], verbose_name=pgettext_lazy('school mark', 'mark')
+    )
+
+    class Meta:
+        verbose_name = gettext_lazy('vote')
+        verbose_name_plural = gettext_lazy('votes')
+        constraints = [
+            models.UniqueConstraint(fields=['completion', 'question', 'teacher'], name='unique_assignment')
+            # TODO: add teacher_belongs_to_group constraint
+        ]
+
+    def __str__(self):
+        return gettext('vote') + ' ' + gettext('No.') + ' ' + str(self.id)
