0ec79fe45c906eb7a9491b36209abb4aafdb6fea
Twixes <dev@twixes.com>
2019-08-07 17:16:59 +0200

    Add error pages

--- antimark/urls.py
+++ antimark/urls.py
@@ -3,8 +3,14 @@
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
+from .settings import DEBUG
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
     path('admin/', admin.site.urls, name='admin')
 ]
+
+if DEBUG:
+    # in DEBUG mode error pages aren't displayed, this allows for working on them even with DEBUG == True
+    for error in (404, 500):
+        urlpatterns.append(path(f'{error}/', TemplateView.as_view(template_name=f'{error}.html')))
