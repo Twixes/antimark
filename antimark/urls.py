4340f65414f55b54f2abcc2673b260f5734ff4b0
Twixes <dev@twixes.com>
2019-08-10 03:33:48 +0200

    Add internationalization JavaScript

--- antimark/urls.py
+++ antimark/urls.py
@@ -3,16 +3,18 @@
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
+from django.views.i18n import JavaScriptCatalog
 from .settings import DEBUG
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
     path('poll/', TemplateView.as_view(template_name='poll.html'), name='poll'),
     path('panel/', TemplateView.as_view(template_name='panel.html'), name='panel'),
-    path('admin/', admin.site.urls, name='admin')
+    path('admin/', admin.site.urls, name='admin'),
+    path('i18n.js', JavaScriptCatalog.as_view(), name='javascript-catalog')
 ]
 
 if DEBUG:
-    # in DEBUG mode error pages aren't displayed, this allows for working on them even with DEBUG == True
+    # in DEBUG mode error pages aren't displayed, this allows for working on them even in that case
     for error in (404, 500):
         urlpatterns.append(path(f'{error}/', TemplateView.as_view(template_name=f'{error}.html')))
